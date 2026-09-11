db0ad87673b9a6516dd825f29c48ee119ea0caf6
DeadPixel <d3ad.pix3l1@gmail.com>
2026-09-11 16:28:41 -0400

    17. Animation Data Added animation data arrays for rifle and pistol - idle, strafe BS, crouch BS, sprint, and Aim Offsets

--- Source/FPS/Public/Data/WeaponData.h
+++ Source/FPS/Public/Data/WeaponData.h
@@ -7,9 +7,38 @@
 #include "Engine/DataAsset.h"
 #include "WeaponData.generated.h"
 
-/**
- * 
- */
+class UAnimSequence;
+
+USTRUCT(BlueprintType)
+struct FPlayerAnims
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UAnimSequence> IdleAnim = nullptr;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UAnimSequence> AimIdleAnim = nullptr;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UAnimSequence> CrouchIdleAnim = nullptr;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UAnimSequence> SprintAnim = nullptr;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UBlendSpace> AimOffset_Hip = nullptr;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UBlendSpace> AimOffset_Aim = nullptr;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UBlendSpace> Strafe_Standing = nullptr;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UBlendSpace> Strafe_Crouching = nullptr;
+};
+
 UCLASS()
 class FPS_API UWeaponData : public UDataAsset
 {
@@ -17,4 +46,10 @@ class FPS_API UWeaponData : public UDataAsset
 public:
 	UPROPERTY(EditDefaultsOnly, Category = "FPS|WeaponData|Weapons")
 	TMap<FGameplayTag, FName> GripPoints;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "FPS|WeaponData|FirstPerson")
+	TMap<FGameplayTag, FPlayerAnims> FirstPersonAnims;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "FPS|WeaponData|ThirdPerson")
+	TMap<FGameplayTag, FPlayerAnims> ThirdPersonAnims;
 };
