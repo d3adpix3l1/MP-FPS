a4132df6c16573123b578eb6d5ed4a14e2f85fc2
DeadPixel <d3ad.pix3l1@gmail.com>
2026-09-25 10:22:22 -0400

    Added code to allow adding ammo to the reserve ammo

--- Source/FPS/Private/Combat/CombatComponent.cpp
+++ Source/FPS/Private/Combat/CombatComponent.cpp
@@ -157,6 +157,31 @@ void UCombatComponent::Notify_ReloadWeapon()
 	}
 }
 
+void UCombatComponent::AddAmmo(const FGameplayTag& WeaponType, int32 AmmoAmount)
+{
+	if (GetOwner->HasAuthority() && !IsValid(CurrentWeapon)) return;
+	
+	if (!ReserveAmmo.Contains(WeaponType))
+	{
+		ReserveAmmo.Add(WeaponType, AmmoAmount);
+	}
+	
+	const int32 NewAmmo = ReserveAmmo.FindChecked(WeaponType) + AmmoAmount;
+	ReserveAmmo[WeaponType] = NewAmmo;
+	
+	if (CurrentWeapon->WeaponType.MatchesTagExact(WeaponType))
+	{
+		CurrentReserveAmmo = NewAmmo;
+		if (CurrentWeapon->Ammo ==  && NewAmmo > 0)
+		{
+			Server_ReloadWeapon();
+		}
+		
+		OnAmmoCounterChanged.Broadcast(CurrentWeapon->GetAmmoCounterDynamicMaterialInstance(), CurrentWeapon->Ammo, CurrentWeapon->MagCapacity);
+		OnCurrentReserveAmmoChanged.Broadcast(CurrentReserveAmmo, CurrentWeapon->Ammo, CurrentWeapon->WeaponIcon);
+	}
+}
+
 void UCombatComponent::Client_ReloadWeapon_Implementation(int32 NewWeaponAmmo, int32 NewCarriedAmmo)
 {
 	APawn* OwningPawn = Cast<APawn>(GetOwner());
